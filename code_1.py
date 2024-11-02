8f194e00fdc787dc1c40fa854af41b4b12162769
Amishi Mishra <mishmish@unc.edu>
2024-11-02 18:42:50 -0400

    hey babs

--- code_1.py
+++ code_1.py
@@ -1,6 +1,7 @@
 """Making the code in our program"""
 
 import getpass
+import json
 import os
 from langchain_openai import ChatOpenAI
 import requests
@@ -9,7 +10,8 @@ os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
 llm = ChatOpenAI(model="gpt-4o-mini",api_key="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")
 
-response = requests.get("https://data.unwrangle.com/api/getter/?platform="lowes_search"")
+def get_products(word):
+    link = 'https://data.unwrangle.com/api/getter/?platform=lowes_search&search='+word+'&api_key=8e2ed113c38dce504bd8557d66cb54719be94205'
+    response = requests.get(link)
+    return response.json()
 
-if response.status_code == 200:
-        products = response.json().get('products', [])
