0450b7343e1c6b6626c7ab23f192f033d1e5b58a
Amishi Mishra <mishmish@unc.edu>
2024-11-02 18:51:39 -0400

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,7 +1,5 @@
 """Making the code in our program"""
 
-import getpass
-import json
 import os
 from langchain_openai import ChatOpenAI
 import requests
@@ -14,4 +12,6 @@ def get_products(word):
     link = 'https://data.unwrangle.com/api/getter/?platform=lowes_search&search='+word+'&api_key=8e2ed113c38dce504bd8557d66cb54719be94205'
     response = requests.get(link)
     return response.json()
+
+
 
