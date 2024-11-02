4ef08242ab25d5e379d2ef267cc30d1d4a938510
Amishi Mishra <mishmish@unc.edu>
2024-11-02 16:01:29 -0400

    yass we cook

---
+++ code.py
@@ -0,0 +1,12 @@
+"""Making the code in our program"""
+
+import getpass
+import os
+from langchain_openai import ChatOpenAI
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["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"] = getpass.getpass()
+os.environ["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"] = getpass.getpass()
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
