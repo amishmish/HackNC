9e1887f93e4ea8413f3f260d7543f10fc6cf550e
Amishi Mishra <mishmish@unc.edu>
2024-11-02 19:54:55 -0400

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {Image, StyleSheet} from 'react-native';
 import './App.css'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cover: {
+    width: 6000,
+    height: 3000,
+    resizeMode: 'cover',
+  },
+});
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +26,11 @@ function App() {
     <>
       <div>
         <a target="_blank">
-          <img src={"https://mobileimages.lowes.com/marketingimages/2b928fb1-a997-4a2c-8824-4b149b75b8ef/lowes-logos-dp18-332098-og.jpg"} className="logo" alt="Vite logo" />
+          <img 
+          src={"https://mobileimages.lowes.com/marketingimages/2b928fb1-a997-4a2c-8824-4b149b75b8ef/lowes-logos-dp18-332098-og.jpg"}
+          className="logo"
+          alt="Lowes logo"
+          style={styles.cover} />
         </a>
       </div>
       {/*<div>
