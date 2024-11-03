cd4e4712ca9023288cd7db86e2e11e0777489411
Amishi Mishra <mishmish@unc.edu>
2024-11-02 22:04:22 -0400

    Update gpt.py

--- React/gpt.py
+++ React/gpt.py
@@ -4,34 +4,21 @@ import getpass
 import os
 import requests
 from langchain_openai import ChatOpenAI
-import pandas as pd
 
 os.environ["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"] = getpass.getpass()
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 
-lowes_word = str()
-project_descript = str()
-cost_list = list()
-total_cost_list = list()
-total_cost = 0
-material_list = list()
-quantity_list = list()
-
 def get_product(word): # gets product from Lowe's API based on word search
     """Returns first product in results of searching for <word> in Lowe's API"""
-    global lowes_word
     link = 'https://data.unwrangle.com/api/getter/?platform=lowes_search&search='+word+'&api_key=8e2ed113c38dce504bd8557d66cb54719be94205'
     response = requests.get(link)
-    lowes_word = word
     return response.json()["Results"][0]
 
 def get_project_materials(project_description): #asks GPT for materials for project input
     """Returns list of all materials needed for <project description> based on GPT's response"""
-    global project_descript
-    link = 'https://data.unwrangle.com/api/getter/?platform=lowes_search&search='+lowes_word+'&api_key=8e2ed113c38dce504bd8557d66cb54719be94205'
+    link = 'https://data.unwrangle.com/api/getter/?platform=lowes_search&search='+project_description+'&api_key=8e2ed113c38dce504bd8557d66cb54719be94205'
     response = llm.generate(f"List the materials needed to build a {project_description} using products from {link} and place each item on a new line.")
-    project_descript = project_description #make this global because it will be used on other places
     return response.split("\n")
 
 def get_project_quantity(materials, project): #asks GPT for materials for project input
